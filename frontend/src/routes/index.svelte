c0966e08612793e46d510c9dedf92d6cc413b480
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2021-10-31 18:54:08 +0100

    Hook frontend up to cell phone endpoint

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,20 +1,13 @@
 <script>
+  import { onMount } from 'svelte';
 	import Footer from '../components/footer.svelte';
 	import Navbar from '../components/navbar.svelte';
 
+  let phones = [];
 	let purchaseTotal = 0;
 	let phoneLines = 0;
 	let selectedPhones = [];
-	let internetConnetion = false;
-	let phones = [
-		{
-			name: 'Motorola G99',
-			price: 800,
-			amount: 4,
-			regularity: 'Once'
-		},
-		{ name: 'iPhone 99', price: 6000, amount: 1, regularity: 'Once' }
-	];
+	let internetConnection = false;
 
 	const updateAmount = async (name, amount) => {
 		for (let i = 0; i < selectedPhones.length; i++) {
@@ -31,7 +24,7 @@
 			cell_phones: undefined
 		};
 
-		if (internetConnetion) {
+		if (internetConnection) {
 			shoppingCart.internet_connection = {
 				price: 200,
 				regularity: 'Monthly'
@@ -50,6 +43,12 @@
 		return shoppingCart;
 	};
 
+	const fetchCellPhones = async () => {
+		return fetch('http://localhost:2000/cellphones')
+			.then((response) => response.json())
+			.then((data) => data)
+	};
+
 	const fetchRunningTotal = async () => {
 		const payload = await buildShoppingCartPayload();
 		fetch('http://localhost:2000/totalprice', {
@@ -60,6 +59,11 @@
 			.then((response) => response.json())
 			.then((data) => (purchaseTotal = data.total_price));
 	};
+
+  onMount(async () => {
+    phones = await fetchCellPhones();
+  })
+
 </script>
 
 <body class="flex flex-col h-screen justify-between">
@@ -71,13 +75,13 @@
 				<div class="card-body">
 					<h2 class="card-title">KEANet</h2>
 					<!-- Internet connection -->
-					<div class="p-6 card bordered">
+					<div class="p-3 card bordered">
 						<div class="form-control">
 							<label class="cursor-pointer label">
 								<span class="label-text">Internet connection</span>
 								<input
 									type="checkbox"
-									bind:checked={internetConnetion}
+									bind:checked={internetConnection}
 									on:change={fetchRunningTotal}
 									class="checkbox checkbox-primary"
 								/>
@@ -85,7 +89,7 @@
 						</div>
 					</div>
 					<!-- Phone line -->
-					<p>Phone lines: {phoneLines}</p>
+					<p class="pt-4">Phone lines: {phoneLines}</p>
 					<input
 						type="range"
 						max="8"
@@ -94,64 +98,66 @@
 						class="range range-primary"
 					/>
 					<!-- Cell phones -->
-					<div class="overflow-x-auto">
-						<table class="table w-full table-compact">
-							<thead>
-								<tr>
-									<th>Name</th>
-									<th>Price</th>
-									<th>Amount</th>
-									<th>Regularity</th>
-									<th>Purchase?</th>
-								</tr>
-							</thead>
-							<tbody>
-								{#each phones as phone, i}
+					<div class="overflow-x-auto pt-4">
+							<table class="table w-full table-compact">
+								<thead>
 									<tr>
-										<td>
-											{phone.name}
-										</td>
-										<td>
-											{phone.price * phone.amount}
-										</td>
-										<td>
-											<select
-												class="select"
-												bind:value={phone.amount}
-												on:change={updateAmount(phone.name, phone.amount)}
-												on:change={fetchRunningTotal}
-											>
-												<option disabled>Amount</option>
-												{#each Array(10) as _, i}
-													<option>{i + 1}</option>
-												{/each}
-											</select>
-										</td>
-										<td>
-											{phone.regularity}
-										</td>
-										<td>
-											<div class="form-control">
-												<label class="cursor-pointer label">
-													<input
-														type="checkbox"
-														checked={false}
-														class="checkbox checkbox-primary"
-														value={phone}
-														bind:group={selectedPhones}
-														on:change={fetchRunningTotal}
-													/>
-												</label>
-											</div>
-										</td>
+										<th>Name</th>
+										<th>Price</th>
+										<th>Amount</th>
+										<th>Regularity</th>
+										<th>Purchase?</th>
 									</tr>
-								{/each}
-							</tbody>
-						</table>
+								</thead>
+								<tbody>
+									{#each phones as phone}
+										<tr>
+											<td>
+												{phone.name}
+											</td>
+											<td>
+												{phone.price * phone.amount}
+											</td>
+											<td>
+												<select
+													class="select"
+													bind:value={phone.amount}
+													on:change={updateAmount(phone.name, phone.amount)}
+													on:change={fetchRunningTotal}
+												>
+													<option disabled>Amount</option>
+													{#each Array(10) as _, i}
+														<option>{i + 1}</option>
+													{/each}
+												</select>
+											</td>
+											<td>
+												{phone.regularity}
+											</td>
+											<td>
+												<div class="form-control">
+													<label class="cursor-pointer label">
+														<input
+															type="checkbox"
+															checked={false}
+															class="checkbox checkbox-primary"
+															value={phone}
+															bind:group={selectedPhones}
+															on:change={fetchRunningTotal}
+														/>
+													</label>
+												</div>
+											</td>
+										</tr>
+									{/each}
+								</tbody>
+							</table>
+					</div>
+					<!-- Total price -->
+					<div class="card text-center pt-4">
+						<h2>Total price: {purchaseTotal} DKK</h2>
 					</div>
 				</div>
-				<!-- Total price -->
-				<h2>Total price: {purchaseTotal} DKK</h2>
 				<!-- Buy button -->
 				<button class="btn btn-primary">Buy</button>
 			</div>
